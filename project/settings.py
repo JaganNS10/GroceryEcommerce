c96cdbfd23c444410f9cea38eb6f4f46199a00e0
JaganNS10 <ns.jagansenthilkumar@gmail.com>
2025-08-05 17:52:09 +0530

    updated

--- project/settings.py
+++ project/settings.py
@@ -144,11 +144,11 @@ import cloudinary
 # Cloudinary credentials (get from https://cloudinary.com/console)
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-}
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+# }
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
