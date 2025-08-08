872aeb10624777a73444891c1d9c29543db10ce8
JaganNS10 <ns.jagansenthilkumar@gmail.com>
2025-08-08 13:45:49 +0530

    updated

--- project/settings.py
+++ project/settings.py
@@ -176,8 +176,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ns.jagansenthilkumar@gmail.com'
-EMAIL_HOST_PASSWORD = 'bdxa tdlw slkf mioj'
+EMAIL_HOST_USER = 'Nammakadi601204@gmail.com'
+EMAIL_HOST_PASSWORD = 'lfvr aobz wefx kggs'
 
 
 
